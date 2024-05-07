23424340cb4998d121684723aecb104a3fc7291a
arky02 <kyean07@naver.com>
2024-05-07 21:38:27 +0900

    style: textArea 높이 조절

--- src/App.jsx
+++ src/App.jsx
@@ -155,7 +155,7 @@ function App() {
           <div className="flex relative justify-between items-center w-full pt-[15px] pb-[10px] px-[25px] shadow-lg shadow-black">
             <motion.div whileTap={{ scale: 0.97 }} className="w-full">
               <textarea
-                className="w-full h-[3.125rem] resize-none rounded-[1.875rem] py-[.625rem] pl-[1.375rem] pr-[2rem] border-[#D6D6D6] bg-[#F9F9F9] border-[.0625rem] my-[5px]"
+                className="w-full h-[47px] resize-none rounded-[1.875rem] py-[.625rem] pl-[1.375rem] pr-[2rem] border-[#D6D6D6] bg-[#F9F9F9] border-[.0625rem] my-[5px]"
                 placeholder={T.InputPlaceholder[0]}
                 value={text}
                 text={text}
@@ -168,7 +168,7 @@ function App() {
 
             <button
               id="send"
-              className="absolute right-[35px] top-[28px] bg-transparent cursor-pointer border-[none]"
+              className="absolute right-[34px] top-[26px] bg-transparent cursor-pointer border-[none]"
               onClick={sendMyText}
               disabled={!isChatValid}
             >
