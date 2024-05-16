392373fb52d447a52a949a2e2f48a4d6ecf4632d
arky02 <kyean07@naver.com>
2024-05-16 18:55:13 +0900

    feat: pretendard 적용

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import ReactGA from "react-ga4";
+import "./assets/font/font.css";
 
 // 구글 애널리틱스 운영서버만 적용
 if (process.env.REACT_APP_GOOGLE_ANALYTICS) {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {},
+  theme: {
+    fontFamily: {
+      sans: ["Pretendard"],
+    },
+    fontWeight: {
+      regular: "400",
+      medium: "500",
+      semiBold: "600",
+      bold: "700",
+    },
+  },
   plugins: [],
 };
